732687d80f4c9d26462d01a49677b87286431052
wakilshinwari <shinwariabdulw@gmail.com>
2022-01-06 21:53:08 -0500

    First Class TestNG

---
+++ TestNgBasicsBatch11/src/class1/FirstExample.java
@@ -0,0 +1,19 @@
+package class1;
+
+import org.testng.annotations.Test;
+
+public class FirstExample {
+    @Test
+    public void firstTest(){
+
+        System.out.println("I am first test");
+    }
+    @Test
+    public void secondTest(){
+        System.out.println(" I am second test");
+    }
+    @Test
+    public void thirdTest(){
+        System.out.println("I am third test");
+    }
+}
